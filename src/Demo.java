9c962f36f220a87cb10d3bbb3462d1b6a21c7fb0
Alexander Ivanov <alexander.ivanov981@gmail.com>
2022-03-24 13:55:50 +0200

    added another line in the Java project

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,7 @@ public class Demo {
     public static void main(String[] args) {
 
         System.out.println("Hello GitBash!");
+        System.out.println("That's a new line for GitBash!");
 
     }
 }
